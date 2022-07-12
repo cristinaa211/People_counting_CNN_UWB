0882eb968a23dacc18ca4ffebef739a5ac7e2824
Cristina_P <61435903+ParaschivaCristina@users.noreply.github.com>
2022-07-12 14:15:10 +0300

    Add files via upload

---
+++ classification_humans_cnn.py
@@ -0,0 +1,310 @@
+import torch
+from torch import nn
+import pytorch_lightning as pl
+import mat73
+import os
+import numpy as np
+from torch.utils.data import DataLoader, random_split, TensorDataset
+import torch.nn.functional as F
+from torchsummary import summary
+from torchshape import tensorshape
+import matplotlib.pyplot as plt
+import torchmetrics 
+import sklearn
+from sklearn import metrics
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def get_data(filename, name = 'dataset', segm = 50,pca = False):
+    
+    file = mat73.loadmat(filename)
+    data_r = file[name]
+    data_r = np.float32(data_r)
+    labels_cnn = data_r[:,-1]
+    data_cnn = data_r[:,:-1]
+    date = []
+    labels = []
+    
+    if pca == True:
+        pca = PCA(n_components=6)
+        data_cnn = pca.fit_transform(data_cnn)
+        
+    for i in range(0, int(data_r.shape[0]/segm)):
+        date.append(data_cnn[segm*i:segm*(i+1),:])
+        labels.append(np.mean(labels_cnn[segm*i:segm*(i+1)]))
+        
+    labels = np.array(labels)
+    labels = labels.reshape(-1,1)   
+    
+    date = np.array(date)
+
+    date = torch.from_numpy(date)
+    date = (date - torch.mean(date))/torch.max(date)
+    labels = torch.from_numpy(labels)
+    
+
+    return date, labels
+
+class Model(pl.LightningModule, nn.Module):
+
+    def __init__(self, lr = 1e-3, no_classes = 21, pca = False):
+        super(Model, self).__init__()
+        self.lr = lr
+        self.pca = pca
+        self.no_classes = no_classes
+        self.loss_vect = []
+        self.acc = []
+        self.precision_vect = []
+        self.recall_vect = []
+        self.f1_score_vect = []
+
+        ## Netowrk for input shape of (1,50,6) -> PCA use
+        if self.pca:
+            self.network = nn.Sequential(
+
+                nn.Conv2d(1, 16, kernel_size = 3),
+                nn.ReLU(),
+                nn.Conv2d(16,1, kernel_size = 3 ),
+                nn.ReLU(),
+                nn.MaxPool2d(2,2),
+                nn.Flatten(),
+                nn.LazyLinear(self.no_classes),
+                nn.Softmax(dim=1)
+            ).cuda()
+        else:
+
+            ## Netowrk for input shape of (1,50,1280)
+            self.network =  nn.Sequential(
+
+                nn.Conv2d(1, 16, kernel_size = 3),
+                nn.ReLU(),
+                nn.Conv2d(16,32, kernel_size = 3 ),
+                nn.ReLU(),
+                nn.MaxPool2d(2,2),
+                nn.Conv2d(32, 64, kernel_size = 3),
+                nn.ReLU(),
+                nn.MaxPool2d(2,2),
+                nn.Conv2d(64, 128, kernel_size = 3),
+                nn.ReLU(),
+                nn.MaxPool2d(2,2),
+                nn.Conv2d(128, 1,kernel_size = 3),
+                nn.ReLU(),
+                nn.MaxPool2d(2,2),
+                nn.Flatten(),
+                nn.LazyLinear(self.no_classes),
+                nn.Softmax(dim=1)
+            ).cuda()
+        #summary(network,(1,50,6))
+        self.loss_func = nn.MSELoss()
+        # self.loss_func = nn.NLLLoss()
+
+    def forward(self, input_data):
+        if input_data.dim() > 3:
+            input_data = torch.squeeze(input_data,dim = 1)
+        elif input_data.dim() == 2:
+            sh1 = input_data.shape[0]
+            sh2 = input_data.shape[1]
+            input_data = torch.reshape(input_data, (1,sh1,sh2))
+        output = self.network(input_data).cuda()
+        output = torch.flatten(output)
+        return output
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y = torch.flatten(y) 
+        out = self.forward(x) 
+        y = y*F.one_hot(y.to(torch.int64), self.no_classes).float()
+        y = torch.flatten(y)
+        loss =self.loss_func(out,y)
+        self.log("train_loss", loss, on_epoch=True)
+        return loss
+
+    def validation_step(self,batch, batch_idx):
+        x, y = batch
+        y = torch.flatten(y) 
+        y = y*F.one_hot(y.to(torch.int64),self.no_classes).float()
+        out = self.forward(x)
+        y = torch.flatten(y)
+        loss =self.loss_func(out,y)
+        self.log('valid_loss', loss, on_epoch=True)
+        return  loss
+
+
+    def validation_epoch_end(self,validation_step_outputs):
+        try:
+            avg_loss = torch.mean(torch.tensor(validation_step_outputs))
+            self.loss_vect.append(avg_loss)
+        except: RuntimeError
+
+    def test_step(self, test_batch, batch_idx ):
+        x, y = test_batch
+        y = torch.flatten(y) 
+        y = y*F.one_hot(y.to(torch.int64),self.no_classes).float()
+        out = self.forward(x)
+        y = torch.flatten(y)
+        loss = self.loss_func(out,y)
+        self.log('test_loss', loss)
+        y_test = y.detach().cpu()
+        y_predict = torch.flatten(out).detach().cpu()
+        
+        return y_test, y_predict
+    
+    def test_step_end(self, output_results):
+        y_test, y_predict = output_results[0], output_results[1]
+        y_test= y_test.reshape(-1,1)
+        y_predict  = y_predict.reshape(-1,1)
+        acc = sklearn.metrics.accuracy_score(y_test,y_predict.round(), normalize=True)
+        prec = sklearn.metrics.precision_score(y_test, y_predict.round(),
+                                                                   average = 'micro', zero_division=1 )
+        rec = sklearn.metrics.recall_score(y_test, y_predict.round(), average = 'micro', zero_division=1)
+        f1 = sklearn.metrics.f1_score(y_test, y_predict.round(), average = 'micro', zero_division=1)
+        self.acc.append(np.mean(acc))
+        self.precision_vect.append(np.mean(prec))
+        self.recall_vect.append(np.mean(rec))
+        self.f1_score_vect.append(np.mean(f1))
+
+    def configure_optimizers(self):
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+
+        return optimizer
+    
+class Data(pl.LightningDataModule):
+    def __init__(self, input_data, batch_size = 16):
+        super(Data, self).__init__()
+        self.input_data = input_data
+        self.batch_size = batch_size
+        train_size = int(0.7 * len(self.input_data))
+        val_size = int(0.5 * (len(self.input_data) - train_size))
+        sizes = (train_size, len(self.input_data) - train_size)
+        sizes_val = (len(self.input_data) - train_size - val_size, val_size)
+
+        self.train_data, self.test_data = random_split(self.input_data, lengths=sizes)
+        self.val_data, self.test_data = random_split(self.test_data, lengths=sizes_val)
+
+    def train_dataloader(self):
+
+        return DataLoader(self.train_data)
+    def val_dataloader(self):
+        return DataLoader(self.val_data)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_data)
+
+class LitDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size):
+        super().__init__()
+        self.batch_size = batch_size
+
+    def train_dataloader(self):
+        return DataLoader(train_dataset, batch_size=self.batch_size)
+
+class Train_model():
+
+    def __init__(self,input_data, targets, nr_epochs, batch_size,  lr = 1e-4, pca = False):
+
+        self.input_data = input_data
+        self.targets = targets
+        self.nr_epochs = nr_epochs
+        self.batch_size = batch_size
+        self.lr = lr
+        self.model = Model(lr = self.lr, pca = pca)
+        self.loss_vect = 0
+
+    def dataset(self):
+        self.input_data = torch.nn.functional.normalize(self.input_data)
+        dataset = TensorDataset(self.input_data, self.targets)
+        data = Data(dataset)
+        return data
+
+    def train(self,model_title):
+
+        trainer = pl.Trainer(gpus=1, max_epochs=self.nr_epochs)
+        trainer.tune(self.model)
+        trainer.fit(self.model, self.dataset())
+        trainer.validate(self.model, self.dataset().val_data)
+        self.loss_vect = getattr(self.model, 'loss_vect')
+        trainer.test(self.model,dataloaders= self.dataset())
+        self.precision_vect = getattr(self.model, 'precision_vect')
+        self.recall_vect = getattr(self.model, 'recall_vect')
+        self.f1_score_vect = getattr(self.model, 'f1_score_vect')
+        self.acc = getattr(self.model, 'acc')
+        print(pl.utilities.model_summary.summarize(self.model, max_depth=1))
+        parameters = list(self.model.parameters())
+        trainer.save_checkpoint("{}.ckpt".format(model_title))
+        
+        return parameters
+
+def choice(choice_dataset = 'raw', filename = r'{}\dataset_few_labels.mat'.format(os.getcwd()) ):
+
+    if choice_dataset == 'pca':
+        date, labels = get_data(filename, pca=True)
+    elif choice_dataset == 'raw':
+        date, labels = get_data(filename)
+
+    return date, labels
+
+
+
+def run_cnn(option = 'raw' , model_title = 'cnn_raw_data', name_csv = 'results_cnn_pytorch',
+            nr_epochs = 50, batch_size = 32, lr = 1e-3):
+    '''
+    Args:
+        option: str
+         'raw' for the entire set of data
+        'pca' for the PCA data compresion
+        model_title: str
+        name_csv: str
+        nr_epochs: int
+            number of epochs
+        batch_size: int
+            the batch size
+        lr: float
+            the learning rate
+
+    Returns:
+        set of data
+        labels
+        list of metrics = the performance of the algorithm
+    '''
+
+    date, labels = choice(option)
+    if option == 'raw':
+        train_model = Train_model(date, labels, pca = False, nr_epochs = nr_epochs, batch_size = batch_size, lr=lr)
+    elif option == 'pca':
+        train_model = Train_model(date, labels, pca = True,  nr_epochs = nr_epochs, batch_size = batch_size, lr=lr)
+
+    train = train_model.train(model_title = model_title)
+
+    # metrics
+
+    precision = np.mean(getattr(train_model, 'precision_vect'))
+    recall = np.mean(getattr(train_model, 'recall_vect'))
+    accuracy = np.mean(getattr(train_model, 'acc'))
+    f1_score = np.mean(getattr(train_model, 'f1_score_vect'))
+
+    lista = [accuracy, recall, precision,f1_score]
+    df = pd.DataFrame(data=np.reshape(lista, (1, 4)), columns=['accuracy', 'f1_score', 'recall', 'precision'])
+    df.to_csv('{}.csv'.format(name_csv), index=False)
+    print(df)
+
+     # loss vs number of iterations
+
+    loss_vect = getattr(train_model, 'loss_vect')
+    plt.figure()
+    plt.plot(range(0, len(loss_vect)), loss_vect)
+    plt.xlabel('Nr iterations')
+    plt.ylabel('Loss')
+    plt.savefig('{}.png'.format(option))
+    plt.show()
+    plt.pause(10)
+    plt.close()
+    return date, labels,lista
+
+
+# date, labels, perf_raw = run_cnn(option= 'raw', nr_epochs=50, model_title='raw_final', name_csv='raw_final')
+date_pca, labels_pca, perf_pca = run_cnn(option= 'pca', nr_epochs=50, model_title='pca_final', name_csv='pca_final')
---
+++ classification_humans_ml.py
@@ -0,0 +1,180 @@
+from secrets import choice
+import numpy as np
+from numpy import random
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import pickle
+from sklearn import metrics
+from sklearn.decomposition import PCA
+from sklearn.metrics import classification_report,precision_score , recall_score , f1_score, confusion_matrix, plot_confusion_matrix, ConfusionMatrixDisplay
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split,cross_val_score
+import tensorflow as tf
+from tensorflow import keras
+import datetime
+from tensorflow.keras import layers, models
+import h5py
+from sklearn import neighbors, svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+import mat73
+import os
+
+
+
+class Load_data():
+    def __init__(self, dir_data='datasets', name = 'date_complet', name2 = 'date_complet', norm = False) :
+        self.dir_data = dir_data
+        self.name = name
+        self.norm = norm
+        self.name2 = name2
+        self.path = os.getcwd()
+
+    def load_data(self):
+
+        dir_data = '{}\{}'.format(self.path, self.dir_data)
+        fname = os.path.join(dir_data, self.name)
+        try: 
+            file = sio.loadmat(fname, mat_dtype = True)
+        except  RuntimeError:
+            file = mat73.loadmat(self.name)
+        data = file[self.name2]
+        labels = data[:,-1]
+        data = np.nan_to_num(data[:, :-1].astype(np.float32))
+        if self.norm == True:
+            data = sklearn.preprocessing.normalize(data[:, :-1].astype(np.float32))
+
+        return data , labels
+
+    def split_data(self, test_size = 0.3):
+        
+        data, labels = self.load_data()
+        x_train, x_test, y_train, y_test = train_test_split( data, labels, test_size= test_size)
+
+        return x_train, y_train, x_test, y_test
+        
+
+class AI_alg():
+    
+    def __init__(self, choice = 'mlp', option = 'raw', x_train = None, y_train= None,
+                 x_test= None, y_test= None, print_results = None,
+                 activation = 'relu', hidden_layer_size = (50,4)):
+        
+        self.choice = choice
+        self.option = option
+        self.x_train = x_train
+        self.y_train = y_train.ravel()
+        self.x_test = x_test
+        self.y_test = y_test.ravel()
+        self.print_results = print_results
+        self.neighbours = 5
+        self.activation = activation
+        self.hidden_layer_size = hidden_layer_size
+
+
+    def choice_model(self):
+        if self.choice == 'knn':
+            model =  neighbors.KNeighborsClassifier(self.neighbours)
+        elif self.choice == 'random_forest':
+            model =  RandomForestClassifier(n_estimators = 300, oob_score=False)
+        elif self.choice == 'svm':
+            model = svm.SVC(kernel='rbf')
+        elif self.choice == 'lsvm':
+            model = svm.SVC()
+        elif self.choice == 'mlp':
+            model = MLPClassifier(max_iter = 200, hidden_layer_sizes= self.hidden_layer_size, activation= self.activation,
+                                   solver='adam', alpha= 1e-3,random_state=0,learning_rate='adaptive')
+        return model
+
+    def forward(self):
+
+        clf = self.choice_model()
+        clf.fit(self.x_train,self.y_train)
+        self.y_pred = clf.predict(self.x_test)
+
+        return clf
+
+    def metrics(self):
+        clf = self.forward()
+        accuracy = clf.score(self.x_test, self.y_test.ravel())
+        cross_val = np.mean(cross_val_score(clf, self.x_test,self.y_test,cv = 10, scoring='f1_macro'))
+        f1 = np.mean(f1_score(self.y_test, self.y_pred, average=None))
+        recall =  np.mean(recall_score(self.y_test, self.y_pred, average = 'weighted' ))
+        precision = np.mean(precision_score(self.y_test, self.y_pred , average = 'micro'))
+        # print('Accuracy :', accuracy)
+        # print('Cross-val ', cross_val )
+        # print('F1 score == ', f1 )
+        # print('Recall == ', recall)
+        # print('Precision score ==', precision)
+        
+        # if self.print_results == True:
+        #     fig, ax = plt.subplots()
+        #     fig.set_size_inches(15, 15, forward=True)
+        #     ConfusionMatrixDisplay.from_estimator(clf, self.x_test, self.y_test,normalize='true', cmap=plt.cm.Blues,ax=ax)
+        #     plt.title('Confusion matrix {}'.format(self.choice))
+        #     plt.show()
+        
+        return accuracy, f1, recall, precision,cross_val
+
+
+    def save_model(self):
+        with open('{}/{}_{}.pkl'.format(os.getcwd(),self.choice, self.option),'wb') as knn:
+            pickle.dump(self.forward(),knn)
+
+
+def get_data(data_r,labels, segm = 50,pca = False):
+    
+    date = []
+    labels = []
+
+    for i in range(0, int(data_r.shape[0]/segm)):
+        date.append(data_r[segm*i:segm*(i+1),:])
+        labels.append(np.mean(labels[segm*i:segm*(i+1)]))
+        
+    labels = np.array(labels)
+    labels = labels.reshape(-1,1)   
+    
+    date = np.array(date)
+    return date, labels
+
+
+def choose_dataset(choice = 'raw', filename = '' , norm = True):
+
+    if choice == 'hybrid_features':
+        load_data = Load_data(norm= norm)
+    if choice == 'pca_hybrid':
+       load_data = Load_data( name = 'pca_set_features' , name2= 'matr',norm= norm)
+
+    data, labels = load_data.load_data()
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.3)
+
+    return data, labels, x_train, x_test, y_train, y_test
+
+choice1 = 'hybrid_features'
+choice2 = 'pca_hybrid'
+
+def run_alg_ml(choice = choice1 , name_csv = 'results_ml'):
+
+    data, labels, x_train, x_test, y_train, y_test = choose_dataset(choice)
+    metrics = []
+    lista = ['knn', 'svm', 'mlp']
+
+    for i in lista:
+        print('I m training the {} model'.format(i))
+        model = AI_alg(choice=i,option = choice, x_train = x_train, y_train = y_train, x_test= x_test, y_test= y_test)
+        model.save_model()
+        accuracy, f1, recall, precision, cross_val = model.metrics()
+        metrics.append([i, accuracy, f1, recall, precision])
+
+    np.reshape(metrics, (len(lista), 5))
+    df = pd.DataFrame(data=metrics,
+                          columns=['model', 'accuracy', 'precision', 'recall','f1_score'])
+    df.to_csv('{}.csv'.format(name_csv), index=False)
+    print(df)
+
+    return data, labels, df
+
+# data,labels, df = run_alg_ml(choice = choice1, name_csv= 'res_ml_compl')
+data_pca,labels_pca, df_pca = run_alg_ml(choice = choice2, name_csv= 'res_ml_pca')
